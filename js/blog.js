afdddd009031f1553ca00253f618ca9e2f25b365
naafiisaa30 <naficste15@gmail.com>
2024-09-25 04:22:28 +0600

    Home page to blog page done

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+document.getElementById('blog-btn').addEventListener('click', function(event){
+
+    //step-2   ----> prevent the reload
+    event.preventDefault(); 
+    window.location.href = '/blog.html';
+
+
+})
+
+// Home button clicked
+document.getElementById('home-btn').addEventListener('click', function(event){
+
+    //step-2   ----> prevent the reload
+    event.preventDefault(); 
+    window.location.href = '/index.html';
+
+
+})
